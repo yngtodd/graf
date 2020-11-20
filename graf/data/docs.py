f8315f8416e79a857fc4b915d06b0d5a0961fca4
yngtodd <young.todd.mk@gmail.com>
2020-11-19 20:03:59 -0500

    Add language model notebook

--- graf/data/docs.py
+++ graf/data/docs.py
@@ -28,15 +28,17 @@ class DocumentGraphs(InMemoryDataset):
                        the raw data
     """
 
-    raw_home = '/Users/ygx/src/python/trans/val_x.pkl'
+    raw_home = "/Users/ygx/src/python/trans/val_x.pkl"
 
-    def __init__(self,
-                 root,
-                 num_vocab,
-                 word_embedding=None,
-                 split="train",
-                 transform=None,
-                 pre_transform=None):
+    def __init__(
+        self,
+        root,
+        num_vocab,
+        word_embedding=None,
+        split="train",
+        transform=None,
+        pre_transform=None,
+    ):
 
         self._check_split(split)
         self.num_vocab = num_vocab
@@ -44,7 +46,7 @@ class DocumentGraphs(InMemoryDataset):
         super(DocumentGraphs, self).__init__(root, transform, pre_transform)
 
     def _check_split(self, split):
-        if split not in ['train', 'valid', 'test']:
+        if split not in ["train", "valid", "test"]:
             raise ValueError(
                 f"Data split must be either `train`, `valid`, or `test`!"
             )
@@ -53,16 +55,18 @@ class DocumentGraphs(InMemoryDataset):
 
     @property
     def raw_file_names(self):
-        return 'data.pkl'
+        return ["val_x.pkl", "val_y.pkl"]
 
     @property
     def processed_file_names(self):
-        return ['train.pt', 'valid.pt', 'test.pt']
+        return ["train.pt", "valid.pt", "test.pt"]
 
     def download(self):
-        shutil.copy(
-            self.raw_home, Path(self.raw_dir).joinpath(self.raw_file_names)
-        )
+        for filename in self.raw_file_names:
+            shutil.copy(
+                self.raw_home,
+                Path(self.raw_dir).joinpath(filename)
+            )
 
     def _load_edge_lists(self, path):
         """load the pickled data
@@ -74,13 +78,15 @@ class DocumentGraphs(InMemoryDataset):
             edges: list of torch long tensors,
                    the edge list for each document graph
         """
-        with open(path, 'rb') as f:
+        p = Path(path)
+
+        with open(p.joinpath('val_x.pkl'), "rb") as f:
             edges = pickle.load(f)
 
-        if self.split == "train":
-            return edges
-        else:
-            raise ValueError('nope!')
+        with open(p.joinpath('val_y.pkl'), "rb") as f:
+            target = pickle.load(f)
+
+        return edges, target
 
     def process(self):
         torch.save(
@@ -88,16 +94,16 @@ class DocumentGraphs(InMemoryDataset):
         )
 
     def process_split(self, split):
-        # Pytorch geometric keeps this as a list of paths,
-        # but we only have only raw graph pickle file
-        raw_path = self.raw_paths[0]
-        edges = self._load_edge_lists(raw_path)
+        edges, target = self._load_edge_lists(self.raw_dir)
+
+        x = random_embeddings(self.num_vocab, 30)
 
         data_list = []
-        for idx, edge_index in enumerate(edges):
+        for doc in range(len(edges)):
             data = Data(
-                x = None,
-                edge_index=edge_index,
+                x=x[doc],
+                y=target[doc],
+                edge_index=edges[doc],
                 num_nodes=self.num_vocab
             )
 
@@ -110,8 +116,6 @@ class DocumentGraphs(InMemoryDataset):
             data_list = [self.pre_transform(d) for d in data_list]
 
         data, slices = self.collate(data_list)
-
-        data.x = random_embeddings(self.num_vocab, 30)
 
         return data
 
@@ -133,13 +137,11 @@ def random_embeddings(vocab_size, embed_dim):
     return m(x)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     from torch_geometric.data import DenseDataLoader
 
-    d = DocumentGraphs('/Users/ygx/data/docs', num_vocab=501)
-    print(f'data: {d}')
-    loader = DenseDataLoader(d,  batch_size=32, shuffle=True)
+    d = DocumentGraphs("/Users/ygx/data/docs", num_vocab=501)
+    loader = DenseDataLoader(d, batch_size=32, shuffle=True)
 
     for data in loader:
         print(data)
-
